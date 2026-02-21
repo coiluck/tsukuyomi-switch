4b4fa1bef03b4fd008318b5b0a4baa1b1484fe7f
coiluck <wvy.wingle@gmail.com>
2026-02-21 18:07:54 +0900

    Fix: prevent default window when fullscreen is true

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,29 @@
+use tauri::Manager;
+use tauri_plugin_store::StoreExt;
+
 use tauri_plugin_prevent_default::Flags;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let store = app.store("settings.json")?;
+            let is_fullscreen = store
+            .get("settings")
+            .and_then(|val| {
+                val.get("screenSize")
+                    .and_then(|size| size.as_str())
+                    .map(|s| s == "fullscreen")
+            })
+            .unwrap_or(false);
+
+            if let Some(window) = app.get_webview_window("main") {
+                window.set_fullscreen(is_fullscreen).unwrap();
+                window.show().unwrap();
+            }
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(prevent_default())
